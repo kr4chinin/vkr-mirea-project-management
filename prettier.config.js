71dbaf9b93ef8db822940e0e67ab4da0245dd633
Ilya Kruchinin <kr4chinin@mail.ru>
2024-05-04 19:51:42 +0300

    chore: uni prettier-plugin-organize-imports due to conflicts with prettier-plugin-tailwindcss

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ['prettier-plugin-tailwindcss', 'prettier-plugin-organize-imports'],
+  plugins: ['prettier-plugin-tailwindcss'],
   tabWidth: 2,
   printWidth: 100,
   singleQuote: true,

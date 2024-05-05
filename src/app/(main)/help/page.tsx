31b9790169660c3485bd8ac8b949887dad97ca29
Ilya Kruchinin <kr4chinin@mail.ru>
2024-05-05 18:55:01 +0300

    chore: help and settings pages mocks

---
+++ src/app/(main)/help/page.tsx
@@ -0,0 +1,11 @@
+import { H2 } from '~/components/ui/typography/h2';
+
+export default async function HelpPage() {
+  return (
+    <div className="flex flex-col gap-4 p-4">
+      <H2>Справка</H2>
+
+      <p>Тут пока ничего нет.</p>
+    </div>
+  );
+}
---
+++ src/app/(main)/settings/page.tsx
@@ -0,0 +1,11 @@
+import { H2 } from '~/components/ui/typography/h2';
+
+export default async function SettingsPage() {
+  return (
+    <div className="flex flex-col gap-4 p-4">
+      <H2>Настройки</H2>
+
+      <p>Тут пока ничего нет.</p>
+    </div>
+  );
+}

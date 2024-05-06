ee17a32308eb8b1180bc9025b667d0536103edc4
Ilya Kruchinin <kr4chinin@mail.ru>
2024-05-06 18:39:44 +0300

    chore: remove looping redirects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import { auth } from '@clerk/nextjs/server';
-import { redirect } from 'next/navigation';
-
 export default async function Home() {
-  const { userId } = auth();
-
-  if (!userId) return redirect('/sign-in');
-
-  return null;
+  return <div>Main page</div>;
 }

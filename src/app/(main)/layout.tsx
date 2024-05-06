f86966be30d710ddcf98f228be848348707e6172
Ilya Kruchinin <kr4chinin@mail.ru>
2024-05-06 19:02:22 +0300

    fix: main layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,15 +1,9 @@
-import { useUser } from '@clerk/nextjs';
-import { redirect } from 'next/navigation';
 import { type ReactNode } from 'react';
 import { api } from '~/trpc/server';
 import { Sidebar } from './_components/sidebar';
 
 export default async function MainLayout({ children }: { children: ReactNode }) {
   const projects = await api.project.getAll();
-
-  const { isSignedIn } = useUser();
-
-  if (isSignedIn) redirect('/sign-in');
 
   return (
     <div className="flex h-full">

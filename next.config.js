2a7c7d8cea44605a5a2cbbfce6fd2f82c489e2f9
Ilya Kruchinin <kr4chinin@mail.ru>
2024-05-06 19:36:39 +0300

    chore: / -> /sign-in redirect

--- next.config.js
+++ next.config.js
@@ -5,6 +5,16 @@
 await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/sign-in',
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default config;

5eb9ed0f67e54073d7529bea91d710f59285efdd
Ilya Kruchinin <kr4chinin@mail.ru>
2024-05-06 18:45:16 +0300

    fix: revert attempt to fix looping redirects changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,3 @@
-import { auth } from '@clerk/nextjs/server';
-import { redirect } from 'next/navigation';
-
 export default async function Home() {
-  const { userId } = auth();
-
-  if (userId) {
-    redirect('/projects');
-  } else {
-    redirect('/sign-in');
-  }
+  return <div>Home page</div>;
 }

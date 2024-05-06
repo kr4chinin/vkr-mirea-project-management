1b88c997988371e1929c8aff5026fb12aae1e445
Ilya Kruchinin <kr4chinin@mail.ru>
2024-05-06 19:00:07 +0300

    fix: attempt to fix looping redirects issue, v5

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,15 +1,15 @@
+import { useUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 import { type ReactNode } from 'react';
 import { api } from '~/trpc/server';
 import { Sidebar } from './_components/sidebar';
-import { auth } from '@clerk/nextjs/server';
-import { redirect } from 'next/navigation';
 
 export default async function MainLayout({ children }: { children: ReactNode }) {
   const projects = await api.project.getAll();
 
-  const { userId } = auth();
+  const { isSignedIn } = useUser();
 
-  if (!userId) redirect('/sign-in');
+  if (isSignedIn) redirect('/sign-in');
 
   return (
     <div className="flex h-full">

247d5a278387ae0b71e37effcc428ef2e7e4d829
Ilya Kruchinin <kr4chinin@mail.ru>
2024-05-06 20:39:33 +0300

    chore: remove user profile page, add user button to header

--- src/app/(auth)/user-profile/[[...user-profile]]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { UserProfile } from '@clerk/nextjs';
-
-export default function UserProfilePage() {
-  return <UserProfile path="/user-profile" />;
-}
--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,27 +1,10 @@
 'use client';
 
-import { SignOutButton, useUser } from '@clerk/nextjs';
+import { SignedIn, UserButton } from '@clerk/nextjs';
 import { BellIcon, CubeIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import { Avatar, AvatarFallback } from '~/components/ui/avatar';
 
 export function Header() {
-  const { isSignedIn, user } = useUser();
-
-  const getAvatarFallback = (): string | null => {
-    if (!user) return null;
-
-    const { firstName, lastName, username } = user;
-
-    if (firstName && lastName) {
-      return `${firstName[0]}${lastName[0]}`;
-    }
-
-    return username;
-  };
-
-  const avatarFallback = getAvatarFallback();
-
   return (
     <header className="flex w-full items-center justify-between gap-4 border-b border-slate-300 bg-white p-4">
       <Link href="/projects" className="flex items-center gap-3">
@@ -30,30 +13,17 @@ export function Header() {
         <h1 className="text-2xl font-bold text-gray-800">Управление Проектами</h1>
       </Link>
 
-      <div className="flex items-center gap-4">
-        {isSignedIn && <SignOutButton redirectUrl="/sign-in" />}
+      <SignedIn>
+        <div className="flex items-center gap-3">
+          <UserButton showName />
 
-        {user && (
-          <div className="flex items-center gap-2">
-            <div className="flex h-[32px] w-[32px] items-center justify-center rounded-full border border-slate-300">
-              <div className="flex h-[20px] w-[20px] shrink-0 items-center justify-center text-slate-500">
-                <BellIcon />
-              </div>
+          <div className="flex h-[28px] w-[28px] items-center justify-center rounded-full border border-slate-300">
+            <div className="flex h-[18px] w-[18px] shrink-0 items-center justify-center text-slate-500">
+              <BellIcon />
             </div>
-
-            {avatarFallback && (
-              <Avatar className="h-[32px] w-[32px] border border-slate-300">
-                <Link
-                  className="flex h-full w-full items-center justify-center"
-                  href="/user-profile"
-                >
-                  <AvatarFallback>{getAvatarFallback()}</AvatarFallback>
-                </Link>
-              </Avatar>
-            )}
           </div>
-        )}
-      </div>
+        </div>
+      </SignedIn>
     </header>
   );
 }

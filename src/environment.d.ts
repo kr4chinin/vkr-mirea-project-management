d4197a1e49fe05976dc89867f9d98e7dfc3502c2
Ilya Kruchinin <kr4chinin@mail.ru>
2024-05-08 19:37:38 +0300

    chore: declare global env vars

---
+++ src/environment.d.ts
@@ -0,0 +1,17 @@
+import "next";
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      readonly DATABASE_URL: string;
+      readonly NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
+      readonly CLERK_SECRET_KEY: string;
+      readonly NEXT_PUBLIC_CLERK_SIGN_IN_URL: string;
+      readonly NEXT_PUBLIC_CLERK_SIGN_UP_URL: string;
+      readonly NEXT_PUBLIC_CLERK_SIGN_IN_FORCE_REDIRECT_URL: string;
+      readonly NEXT_PUBLIC_CLERK_SIGN_UP_FORCE_REDIRECT_URL: string;
+      readonly NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL: string;
+      readonly NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL: string;
+    }
+  }
+}

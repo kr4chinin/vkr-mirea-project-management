b3aef9ce77533fea3641ea47ab1dbcfb070b3197
Ilya Kruchinin <kr4chinin@mail.ru>
2024-05-12 19:26:29 +0300

    feat: link to gh

--- src/app/(main)/help/page.tsx
+++ src/app/(main)/help/page.tsx
@@ -1,3 +1,6 @@
+import { GitHubLogoIcon } from '@radix-ui/react-icons';
+import Link from 'next/link';
+import { Button } from '~/components/ui/button';
 import { H2 } from '~/components/ui/typography/h2';
 
 export default async function HelpPage() {
@@ -5,7 +8,14 @@ export default async function HelpPage() {
     <div className="flex flex-col gap-4 p-4">
       <H2>Справка</H2>
 
-      <p>Тут пока ничего нет.</p>
+      <Button asChild>
+        <Link href="https://github.com/kr4chinin/vkr_mirea" className="flex items-center gap-2">
+          <span className="flex h-[16px] w-[16px] shrink-0 items-center justify-center">
+            <GitHubLogoIcon />
+          </span>
+          GitHub
+        </Link>
+      </Button>
     </div>
   );
 }

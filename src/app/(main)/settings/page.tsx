a58150f58e1b8a95a2d39e5c0297c47bbebeb3ae
Ilya Kruchinin <kr4chinin@mail.ru>
2024-05-12 18:22:54 +0300

    feat: display users on settings page

--- src/app/(main)/settings/page.tsx
+++ src/app/(main)/settings/page.tsx
@@ -1,11 +1,25 @@
 import { H2 } from '~/components/ui/typography/h2';
+import { H3 } from '~/components/ui/typography/h3';
+import { UserBlock } from '~/components/ui/user-block';
+import { api } from '~/trpc/server';
 
 export default async function SettingsPage() {
+  const users = await api.user.getAll();
+
   return (
-    <div className="flex flex-col gap-4 p-4">
+    <div className="flex flex-col gap-6 p-4">
       <H2>Настройки</H2>
 
-      <p>Тут пока ничего нет.</p>
+      <H3>Пользователи системы</H3>
+
+      <ol className="flex flex-col gap-4">
+        {users.map((u, idx) => (
+          <li key={u.id} className="flex items-center gap-4">
+            <span>{idx + 1}.</span>
+            <UserBlock imageUrl={u.imageUrl} lastName={u.lastName} firstName={u.firstName} />
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
